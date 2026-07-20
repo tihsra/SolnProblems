1093ce0a9fd33e7a48f4d4b221d3aeb7e4972552
tihsra <studystuffforlife@gmail.com>
2026-07-20 17:51:54 +0530

    Completed 7 out of 10 questions as of now

---
+++ Miscellaneous/DP/atz/strings/4itr.java
@@ -0,0 +1,107 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while(t-->0){
+			
+			String a = scn.next();
+			String b = new StringBuilder(a).reverse().toString();
+
+			solve(a,b);
+			
+		}
+	}
+
+	public static void solve(String a, String b){
+
+		ps(solver(a,b));
+
+	}
+
+	public static int solver(String a, String b){
+
+		int memo[][] = new int[a.length()+1][b.length()+1]; //  the resonsing is that we would have to manually write base case if we use a.lngth and b.length but here no need
+
+		for(int i=1;i<=a.length();i++){
+			for(int j=1;j<=b.length();j++){
+				if(a.charAt(i-1)==b.charAt(j-1)){
+					memo[i][j] = 1+memo[i-1][j-1];
+				}
+				else{
+					memo[i][j] = Math.max(memo[i-1][j],memo[i][j-1]);
+				}
+			}
+		}
+		return memo[a.length()][b.length()];
+	}
+
+	static int MOD = 1000000007;
+	static int IMAX = Integer.MAX_VALUE;
+	static long LMAX = Long.MAX_VALUE;
+	static int IMIN = Integer.MIN_VALUE;
+	static long LMIN = Long.MIN_VALUE;
+	static void ps(Object x){System.out.println(x);}
+	static void pns(Object o){System.out.print(o);}
+	static void inarr(int[] arr, Scanner scn){
+		for(int i = 0; i < arr.length; i++){
+			 int temp = scn.nextInt();
+			 arr[i] = temp;
+		}
+	}
+	static void lnarr(long[] arr, Scanner scn){
+		for(int i = 0; i < arr.length; i++){
+			long temp = scn.nextLong();
+			arr[i] = temp;
+		}
+	}  
+	static class Pair implements Comparable<Pair>{
+		int x;
+		int y;
+		Pair(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+		public int compareTo(Pair o){
+			return Integer.compare(this.x, o.x);
+		}
+	}
+	static class Tuple implements Comparable<Tuple>{
+		int x;
+		int y;
+		int z;
+		Tuple(int x, int y, int z){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+		public int compareTo(Tuple o){
+			return this.x-o.x;
+		}
+	}
+	static long lcm(long a, long b){
+		return (a*b)/gcd(a,b);
+	}
+	static long gcd(long a, long b){
+		while(a!=0){
+			long temp = a;
+			a = b%a;
+			b = temp;
+		}
+		return b;
+	}
+	static long sumfind(long start, long end){
+		return ((end-start+1)*(end+start)/2);
+	}
+	static void mergeSort(long[]a){long[]t=new long[a.length];ms(a,t,0,a.length-1);}
+	static void ms(long[]a,long[]t,int l,int r){
+		if(l>=r)return;
+		int m=(l+r)>>1;
+		ms(a,t,l,m);ms(a,t,m+1,r);
+		int i=l,j=m+1,k=l;
+		while(i<=m&&j<=r)t[k++]=a[i]<=a[j]?a[i++]:a[j++];
+		while(i<=m)t[k++]=a[i++];
+		while(j<=r)t[k++]=a[j++];
+		for(i=l;i<=r;i++)a[i]=t[i];
+	}
+}
