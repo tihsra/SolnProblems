e7ece99b8a4d00ecf40158639c49c630108d5c46
tihsra <studystuffforlife@gmail.com>
2026-07-22 22:07:26 +0530

    Started MCM

---
+++ Miscellaneous/DP/atz/mcm/Main.java
@@ -0,0 +1,128 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while(t-->0){
+			
+			int n = scn.nextInt();
+			int arr[] = new int[n];
+
+			inarr(arr,scn);
+
+			solve(n,arr);
+			
+		}
+	}
+
+	// array represent matrix via reduced dimention method
+	// since for AxB dim A -> R1C1, dim B -> R2C2 -> C1==R2
+	// Therefore C1,R2 are trucatable 
+
+	// Thus cannot change order of the evaluation meaing cannot multipy i0 with i5
+	// only scenario where this is possible is where elements are same
+
+
+	// therefore the answer depends on what we should evaluate first
+	// strictly in the order as given in input
+
+	public static void solve(int n, int arr[]){
+
+		int memo[][] = new int[n][n];
+
+		for(int partArr[] : memo) Arrays.fill(partArr,-1);
+
+		ps(solver(n,arr,1,n-1,memo)); 
+
+	}
+
+	public static int solver(int n, int arr[], int sp, int ep, int memo[][]){  
+
+		if(ep<=sp) return 0; 
+
+		if(memo[sp][ep]!=-1) return memo[sp][ep];
+
+		int ans = IMAX;
+		
+		for(int i=sp;i<ep;i++){
+			
+			int thisAnswer = solver(n,arr,sp,i,memo) + solver(n,arr,i+1,ep,memo) + arr[sp-1]*arr[i]*arr[ep];
+			ans = Math.min(ans,thisAnswer);
+
+		}
+
+		memo[sp][ep] = ans;
+
+		return memo[sp][ep];
+
+	}
+
+	static int MOD = 1000000007;
+	static int IMAX = Integer.MAX_VALUE;
+	static long LMAX = Long.MAX_VALUE;
+	static int IMIN = Integer.MIN_VALUE;
+	static long LMIN = Long.MIN_VALUE;
+	static void ps(Object x){System.out.println(x);}
+	static void pns(Object o){System.out.print(o);}
+	static void inarr(int[] arr, Scanner scn){
+		for(int i = 0; i < arr.length; i++){
+			 int temp = scn.nextInt();
+			 arr[i] = temp;
+		}
+	}
+	static void lnarr(long[] arr, Scanner scn){
+		for(int i = 0; i < arr.length; i++){
+			long temp = scn.nextLong();
+			arr[i] = temp;
+		}
+	}  
+	static class Pair implements Comparable<Pair>{
+		int x;
+		int y;
+		Pair(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+		public int compareTo(Pair o){
+			return Integer.compare(this.x, o.x);
+		}
+	}
+	static class Tuple implements Comparable<Tuple>{
+		int x;
+		int y;
+		int z;
+		Tuple(int x, int y, int z){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+		public int compareTo(Tuple o){
+			return this.x-o.x;
+		}
+	}
+	static long lcm(long a, long b){
+		return (a*b)/gcd(a,b);
+	}
+	static long gcd(long a, long b){
+		while(a!=0){
+			long temp = a;
+			a = b%a;
+			b = temp;
+		}
+		return b;
+	}
+	static long sumfind(long start, long end){
+		return ((end-start+1)*(end+start)/2);
+	}
+	static void mergeSort(long[]a){long[]t=new long[a.length];ms(a,t,0,a.length-1);}
+	static void ms(long[]a,long[]t,int l,int r){
+		if(l>=r)return;
+		int m=(l+r)>>1;
+		ms(a,t,l,m);ms(a,t,m+1,r);
+		int i=l,j=m+1,k=l;
+		while(i<=m&&j<=r)t[k++]=a[i]<=a[j]?a[i++]:a[j++];
+		while(i<=m)t[k++]=a[i++];
+		while(j<=r)t[k++]=a[j++];
+		for(i=l;i<=r;i++)a[i]=t[i];
+	}
+}
